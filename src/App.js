adcba84c84963918e9fac26e16b3e9637f98be77
nkfab <fabjeezy@gmail.com>
2020-05-12 12:04:00 +0200

    :building_construction: Setup environment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="jumbotron">
+      <h1>Welcome here</h1>
+      <p>This is it</p>
+    </div>
+  );
+}
+
+export default App;
